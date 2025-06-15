56d10f89fc8d61324a5937e900f92dd3e0f38b8c
Sean Murphy <sean@iamseanmurphy.com>
2025-06-15 00:43:45 +0000

    2.1

--- wpecounter/includes/class-views.php
+++ wpecounter/includes/class-views.php
@@ -52,10 +52,66 @@ if (!class_exists('WPeCounterViews')) {
 
 			/* Admin filters    */
 			add_action('admin_init', array($this, 'admin_init'));
+			add_action('add_meta_boxes', array(__CLASS__, 'add_views_metabox'));
+		}
+
+
+		public static function add_views_metabox(){
+			$options	 = get_option('WPeCounter_Options'); // $WPeCounterSettings->options_key);
+			$cpostypes	 = $options['cpostypes'];
+			$screens = array();
+			$args		 = array('public' => true);
+			$output		 = 'names'; // names or objects
+			$post_types	 = get_post_types($args, $output);
+
+			foreach ($post_types as $post_type) {
+				if (@$cpostypes[$post_type]) {
+					$screens[] = $post_type;
+				}
+			}
+			add_meta_box(
+				'views_metabox',
+				__('Views Counter', 'wpecounter'),
+				array(__CLASS__, 'render_views_metabox'),
+				$screens,
+				'side',
+				'default'
+			);
 		}
 
 		/**
-		 * Adds support for 'wpecounter' to the 'post', 'page', and 'attachment' post types (default WordPress 
+		 * Renders the views metabox.
+		 *
+		 * @param WP_Post $post The current post object.
+		 */
+		public static function render_views_metabox($post) {
+
+			// Print styles once
+			self::print_reset_views_button_styles();
+
+			// Prepare nonce and button
+			$nonce = wp_create_nonce('wpecounter_reset_views_' . $post->ID);
+			$reset_button = sprintf(
+				'<a href="#" class="wpecounter-reset-views-btn editor-post-publish-button editor-post-publish-button__button button-primary is-compact" data-postid="%d" data-nonce="%s" title="%s">%s</a>',
+				esc_attr($post->ID),
+				esc_attr($nonce),
+				esc_attr__('Reset Views', 'wpecounter'),
+				esc_attr__('Reset', 'wpecounter')
+			);
+
+			// Get views count
+			$views = self::get_post_views_count($post->ID);
+
+			// Output metabox content
+			printf(
+				'<div class="wpecounter-views-box"><label id="wpecounter-views-count" class="wpecounter-views-label">%s </label>%s</div>',
+				sprintf('%d', $views),
+				$reset_button
+			);
+		}
+
+		/**
+		 * Adds support for 'wpecounter' to the 'post', 'page', and 'attachment' post types (default WordPress
 		 * post types).  For all other post types, the theme should explicitly register support for this feature.
 		 *
 		 * @since  1.0.0
@@ -154,19 +210,45 @@ if (!class_exists('WPeCounterViews')) {
 		 * @return void
 		 */
 		function update_single_views($post_id) {
-			/* If we're on a singular view of a post, calculate the number of views. */
-			if (!empty($post_id)) {
+			
+			// Get plugin options from the database
+			$options = get_option('WPeCounter_Options');
+			
+			// Get the list of roles for which views should be counted
+			$not_allowed_roles = isset($options['views_counter_rol']) ? (array)$options['views_counter_rol'] : array();
 
-				/* Allow devs to override the meta key used. By default, this is 'Views'. */
+			// Flag to determine if the view should be counted
+			$should_count = false;
+
+			// If role-based counting is enabled and not set to 'all_roles'
+			// Always count views for guests (not logged in)
+			if (!is_user_logged_in()) {
+				$should_count = true;
+			} else {
+				// Only count for logged-in users not in the not allowed roles
+				if (!empty($not_allowed_roles) && is_array($not_allowed_roles) && !in_array('all_roles', $not_allowed_roles)) {
+					$current_user = wp_get_current_user();
+					// If the current user's roles intersect with the not allowed roles, do not count the view
+					if (!empty($current_user->roles) && array_intersect($current_user->roles, $not_allowed_roles)) {
+						$should_count = false;
+					} else {
+						$should_count = true;
+					}
+				} else {
+					// If no roles are restricted, count the view for all logged-in users
+					$should_count = false;
+				}
+			}
+
+			// If the view should be counted and the post ID is valid
+			if ($should_count && !empty($post_id)) {
+				// Get the meta key for storing views
 				$meta_key = $this->wpecounter_views_meta_key();
-
-				/* Get the number of views the post currently has. */
+				// Get the current number of views
 				$old_views = get_post_meta($post_id, $meta_key, true);
-
-				/* Add +1 to the number of current views. */
+				// Increment the view count by 1
 				$new_views = absint($old_views) + 1;
-
-				/* Update the view count with the new view count. */
+				// Update the post meta with the new view count
 				update_post_meta($post_id, $meta_key, $new_views, $old_views);
 			}
 		}
@@ -236,13 +318,96 @@ if (!class_exists('WPeCounterViews')) {
 			echo '<script type="text/javascript">/* <![CDATA[ */ jQuery(document).ready( function() { jQuery.post( "' . admin_url('admin-ajax.php') . '", { action : "entry_views", _ajax_nonce : "' . $nonce . '", post_id : ' . absint($this->post_id) . ' } ); } ); /* ]]> */</script>' . "\n";
 		}
 
-		/**
-		 * Init filters to display column and order by views on post types lists.
-		 *
-		 * @access public
-		 * @return object
-		 */
+// 		/**
+// 		 * Init filters to display column and order by views on post types lists.
+// 		 *
+// 		 * @access public
+// 		 * @return object
+// 		 */
+// 		public function admin_init() {
+// 			global $options;
+// 			$options	 = get_option('WPeCounter_Options'); // $WPeCounterSettings->options_key);
+// 			$cpostypes	 = $options['cpostypes'];
+
+// 			$args		 = array('public' => true);
+// 			$output		 = 'names'; // names or objects
+// 			$post_types	 = get_post_types($args, $output);
+// 			foreach ($post_types as $post_type) {
+// 				if (@$cpostypes[$post_type]) {
+// 					//	add_filter('manage_'.$post_type.'_posts_columns', array( $this, 'posts_columns_id'), 5);
+// 					add_filter('manage_edit-' . $post_type . '_columns', array($this, 'posts_columns_id'), 10);
+// 					add_action('manage_' . $post_type . '_posts_custom_column', array($this, 'posts_custom_id_columns'), 10, 2);
+// 					//Order
+// 					add_filter('manage_edit-' . $post_type . '_sortable_columns', array($this, 'views_column_register_sortable'));
+// 				}
+// 			}
+// 			add_action('pre_get_posts', array($this, 'views_column_orderby'));
+// //			add_action('parse_query', array($this, 'views_column_orderby'));
+// 			add_action('admin_head', array($this, 'post_views_column_width'));
+// //			
+// //			
+// //			add_filter('manage_edit-' . $post_type . '_columns', array(__CLASS__, 'set_edit_wpematico_columns'));
+// //			add_action('manage_' . $post_type . '_posts_custom_column', array(__CLASS__, 'custom_wpematico_column'), 10, 2);
+// //			add_filter("manage_edit-' . $post_type . '_sortable_columns", array(__CLASS__, "sortable_columns"));
+// //			add_action('pre_get_posts', array(__CLASS__, 'column_orderby'));
+// //			
+// 		}
+// 		public function posts_columns_id($columns) {
+// 			global $options;
+// 			$cfg = $options;
+			
+// 			$column_post_views	 = array('post_views' => '' . __('Views', 'wpecounter') . '');
+// 			$column_pos = (isset($cfg['wpecounter_column_pos']) and $cfg['wpecounter_column_pos'] > 0 ) ? $cfg['wpecounter_column_pos'] : 5;
+// 			$columns = array_slice($columns, 0, $column_pos, true) + $column_post_views + array_slice($columns, $column_pos, NULL, true);
+// 			$columns			 = array_merge($columns, $column_post_views);
+// 			return $columns;
+// 		}
+
+		public function posts_custom_id_columns($column_name, $id) {
+			if ($column_name === 'post_views') {
+				echo '' . wpecounter_post_views(array('post_id' => $id)) . '';
+			}
+		}
+		public static function print_reset_views_button_styles() {
+			echo '<style>
+				.wpecounter-views-box{
+					display: flex;
+					align-items: flex-end;
+					justify-content: center;
+					text-align: center;
+				}
+				.wpecounter-views-box .button-primary.wpecounter-reset-views-btn {
+					vertical-align: -webkit-baseline-middle; 
+					font-size: 10px; 
+					min-height: 20px; 
+					line-height: 2; 
+					margin-left: 15px;
+				}
+				.wpecounter-views-label{
+					display: inline-flex;
+					font-size: 60px;
+					font-weight: bold;
+					color: grey;
+					line-height: 45px;
+				}
+			</style>';
+		}
+
+		// Add the new column to the posts table
+		public function posts_columns_id($columns) {
+			global $options;
+			$cfg = $options;
+
+			$column_post_views = array('post_views' => '' . __('Views', 'wpecounter') . '');
+			$column_pos = (isset($cfg['wpecounter_column_pos']) and $cfg['wpecounter_column_pos'] > 0 ) ? $cfg['wpecounter_column_pos'] : 5;
+			$columns = array_slice($columns, 0, $column_pos, true) + $column_post_views + array_slice($columns, $column_pos, NULL, true);
+			$columns = array_merge($columns, $column_post_views);
+			return $columns;
+		}
+
+		// Enqueue admin JS for AJAX reset
 		public function admin_init() {
+			global $options;
 			$options	 = get_option('WPeCounter_Options'); // $WPeCounterSettings->options_key);
 			$cpostypes	 = $options['cpostypes'];
 
@@ -251,38 +416,39 @@ if (!class_exists('WPeCounterViews')) {
 			$post_types	 = get_post_types($args, $output);
 			foreach ($post_types as $post_type) {
 				if (@$cpostypes[$post_type]) {
-					//	add_filter('manage_'.$post_type.'_posts_columns', array( $this, 'posts_columns_id'), 5);
 					add_filter('manage_edit-' . $post_type . '_columns', array($this, 'posts_columns_id'), 10);
 					add_action('manage_' . $post_type . '_posts_custom_column', array($this, 'posts_custom_id_columns'), 10, 2);
-					//Order
 					add_filter('manage_edit-' . $post_type . '_sortable_columns', array($this, 'views_column_register_sortable'));
 				}
 			}
 			add_action('pre_get_posts', array($this, 'views_column_orderby'));
-//			add_action('parse_query', array($this, 'views_column_orderby'));
 			add_action('admin_head', array($this, 'post_views_column_width'));
-//			
-//			
-//			add_filter('manage_edit-' . $post_type . '_columns', array(__CLASS__, 'set_edit_wpematico_columns'));
-//			add_action('manage_' . $post_type . '_posts_custom_column', array(__CLASS__, 'custom_wpematico_column'), 10, 2);
-//			add_filter("manage_edit-' . $post_type . '_sortable_columns", array(__CLASS__, "sortable_columns"));
-//			add_action('pre_get_posts', array(__CLASS__, 'column_orderby'));
-//			
+
+			// Enqueue JS only on post list screens
+			add_action('admin_enqueue_scripts', function ($hook) {
+				wp_enqueue_script('wpecounter-reset-views', plugins_url('../assets/js/reset-views.js', __FILE__), array('jquery'), null, true);
+				wp_localize_script('wpecounter-reset-views', 'wpecounterResetViews', array(
+					'ajax_url' => admin_url('admin-ajax.php'),
+					'confirm'  => __('Are you sure you want to reset the views for this post?', 'wpecounter'),
+				));
+			});
+			add_action('wp_ajax_wpecounter_reset_views', array($this, 'ajax_reset_views'));
 		}
 
-		public function posts_columns_id($columns) {
-
-			$column_post_views	 = array('post_views' => '' . __('Views', 'wpecounter') . '');
-			// 5to lugar
-			//$columns = array_slice( $columns, 0, 5, true ) + $column_post_views + array_slice( $columns, 5, NULL, true );
-			$columns			 = array_merge($columns, $column_post_views);
-			return $columns;
-		}
-
-		public function posts_custom_id_columns($column_name, $id) {
-			if ($column_name === 'post_views') {
-				echo '' . wpecounter_post_views(array('post_id' => $id)) . '';
+		// AJAX handler to reset views
+		public function ajax_reset_views() {
+			if (
+				!isset($_POST['post_id'], $_POST['nonce']) ||
+				!wp_verify_nonce($_POST['nonce'], 'wpecounter_reset_views_' . absint($_POST['post_id']))
+			) {
+				wp_send_json_error(array('message' => __('Invalid request.', 'wpecounter')));
 			}
+			$post_id = absint($_POST['post_id']);
+			if ($post_id) {
+				delete_post_meta($post_id, $this->wpecounter_views_meta_key());
+				wp_send_json_success(array('message' => __('Views reset.', 'wpecounter')));
+			}
+			wp_send_json_error(array('message' => __('Invalid post ID.', 'wpecounter')));
 		}
 
 		public function views_column_register_sortable($columns) {
--- wpecounter/readme.txt
+++ wpecounter/readme.txt
@@ -1,57 +1,43 @@
 === WP Views Counter ===
-Contributors: etruel, khaztiel, gerarjos14  
-Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7267TH4PT3GSW  
-Tags: views, count visits, post views, post visits, ajax counter  
-Requires at least: 3.1  
-Tested up to: 6.8  
-Requires PHP: 5.6  
-Stable tag: 2.0.4  
-License: GPLv2  
+Contributors: etruel, khaztiel, gerarjos14
+Donate link: https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=7267TH4PT3GSW
+Tags: post views, views counter, popular posts, ajax counter, analytics
+Requires at least: 3.1
+Tested up to: 6.8.2
+Requires PHP: 5.6
+Stable tag: 2.1
+License: GPLv2
 
-A lightweight and powerful post views counter. Displays visits in post lists and via shortcode or widget. Ideal for tracking popularity across all post types.
+Fast, lightweight post views counter. Display views in admin, blocks or shortcodes — no tracking scripts required.
 
 == Description ==
 
-**WP Views Counter** lets you see how many views each post, page or custom post type entry has directly in the WordPress admin list table or with a shortcode.
+**WP Views Counter** is a lightweight, high-performance plugin that accurately tracks and displays post, page, and custom post type views — directly in the WordPress admin, via shortcode, or with a Gutenberg block.
 
-Perfect for bloggers, marketers and eCommerce site owners, this plugin is designed for speed and simplicity. Whether you're running a WooCommerce shop, an Easy Digital Downloads store, or a content-heavy blog — you'll benefit from knowing what content your users engage with the most.
+Built for bloggers, marketers, store owners, and developers, it works seamlessly across all post types — including WooCommerce and Easy Digital Downloads — with minimal impact on your site’s speed. No external scripts. No unnecessary bloat.
 
-The counter is AJAX-based, making it efficient and non-intrusive, even on high-traffic sites.
+This plugin does one job and does it exceptionally well: it tells you which content is getting the most attention.
 
-= Why choose WP Views Counter? =
+= Key Benefits =
 
-✅ **Fast and lightweight** — built for performance  
-✅ **Seamless integration** with all custom post types  
-✅ **Easy Digital Downloads compatible**  
-✅ **Popular posts widget included**  
-✅ **Counts views via shortcode or in admin columns**  
-✅ **Multilingual ready**
+✅ **Accurate view counts** in admin columns, shortcode, or block  
+✅ **Metabox per post** with real-time views and reset button  
+✅ **Exclude views from logged-in users or specific roles**  
+✅ **Fully AJAX-powered** — no page reloads or slowdowns  
+✅ **Works with all post types**, including EDD and WooCommerce  
+✅ **Block to display popular posts** — no legacy widgets required  
+✅ **Developer-friendly and fully translatable**
+✅ **Import views from other plugins**
 
-Unlike bloated analytics plugins, WP Views Counter focuses only on what matters: showing you the view count where you need it.
+Whether you're optimizing your content strategy or simply want to know what's working, **WP Views Counter** is the simple and effective alternative to bloated analytics plugins.
 
-Developer-friendly on GitHub: https://github.com/Etruel-Developments/wpecounter/issues  
-We welcome forks, feedback and pull requests.
+📦 Start tracking your most popular content today — with clarity, speed and control.
 
-== Features ==
-
-* Display post views in the admin post list columns.
-* Count views on any custom post type.
-* Shortcode `[WPeCounter]` to display views anywhere.
-* AJAX-based counting — no page reloads needed.
-* Order admin lists by view count.
-* Easy Digital Downloads (EDD) integration: track views for Downloads.
-* Legacy widget to show most visited posts or products.
-* Import views from other counters.
-* Fully translatable — multilingual support out of the box.
-
-== Coming Soon ==
-
-* Option to ignore visits from logged-in users or specific roles.
-* Choose display position of Views column per post type.
+💡 Developer-friendly: [Contribute on GitHub](https://github.com/Etruel-Developments/wpecounter/issues) — forks and pull requests welcome.
 
 == Frequently Asked Questions ==
 
-= Can I upgrade from an older version without losing data? =  
+= Can I upgrade from an older version without losing data? =
 Yes. Version 2.0+ automatically imports your previous data and settings. You can also manually import custom view fields. Always make a backup first.
 
 == Installation ==
@@ -68,6 +54,13 @@ Yes. Version 2.0+ automatically imports your previous data and settings. You can
 3. Widget showing popular posts by views.
 
 == Changelog ==
+
+= 2.1 – Jun 13, 2025 =
+* Added a post metabox showing view count with a "Reset" button per post.(Deprecated Legacy Widget will be deleted on future release.)
+* Introduced a Gutenberg block to replace the legacy popular posts widget.
+* New option to choose the Views column position in each post type list.
+* Added feature to exclude logged-in users (or by role) from the view count.
+* New tools to reset all view counters or by post type.
 
 = 2.0.4 – Jun 4, 2025 =
 * Improved output sanitization and escaping for enhanced security.
@@ -120,5 +113,5 @@ Yes. Version 2.0+ automatically imports your previous data and settings. You can
 Initial Release.
 
 == Upgrade Notice ==
-= 2.0.4 =
-Recommended security and stability update. Compatible with WordPress 6.8.
+= 2.1 =
+Major update: Adds Gutenberg block, per-post view metabox with reset, role-based view filtering, and tools to reset counters. Fully compatible with WP 6.8.2.
